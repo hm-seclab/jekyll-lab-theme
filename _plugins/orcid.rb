98e2d0eeb977303a8e07eb573998933cf9091717
Florian Ritterhoff <florian@ritterhoff.org>
2021-05-17 15:13:17 +0200

    Change persons to team

--- _plugins/orcid.rb
+++ _plugins/orcid.rb
@@ -2,21 +2,23 @@ require 'net/http'
 require 'json'
 
 Jekyll::Hooks.register :site, :post_read do |site|
-    
-    site.collections['persons'].docs.each do |person|
-        if person.data['orcid']                
-            Jekyll.logger.info "Requesting ORCID Data for #{person.data['orcid']}"
-            uri = URI("https://pub.orcid.org/v2.1/#{person.data['orcid']}")
-            request = Net::HTTP::Get.new(uri)
-            request["Accept"] = "application/json"
-            response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
-                http.request(request)
-            }
-            
-            orcData = JSON.load(response.body)
-            person.data['orcData'] = orcData 
-            person.data['familyName'] = orcData['person']['name']['family-name']['value']
-            person.data['givenNames'] = orcData['person']['name']['given-names']['value']
+
+    if site.collections['team'] 
+        site.collections['team'].docs.each do |person|
+            if person.data['orcid']                
+                Jekyll.logger.info "Requesting ORCID Data for #{person.data['orcid']}"
+                uri = URI("https://pub.orcid.org/v2.1/#{person.data['orcid']}")
+                request = Net::HTTP::Get.new(uri)
+                request["Accept"] = "application/json"
+                response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
+                    http.request(request)
+                }
+                
+                orcData = JSON.load(response.body)
+                person.data['orcData'] = orcData 
+                person.data['familyName'] = orcData['person']['name']['family-name']['value']
+                person.data['givenNames'] = orcData['person']['name']['given-names']['value']
+            end
         end
     end
 
