f23d0b6658fc216adcf8a17e1e4a5589de774810
Florian Ritterhoff <florian@ritterhoff.org>
2021-05-31 09:46:22 +0200

    Collect publications from orcid by description

--- _plugins/orcid.rb
+++ _plugins/orcid.rb
@@ -1,14 +1,40 @@
 require 'net/http'
 require 'json'
 
-API_BASEURL = "https://pub.orcid.org/v3.0/"
+API_BASEURL = "https://pub.orcid.org/v3.0"
+
+def handlePublications(orcData)
+    
+    orcData['activities-summary']['works']['group'].each do |workGroup| 
+        workGroup['work-summary'].each do |summary| 
+            uri = URI("#{API_BASEURL}#{summary['path']}")
+            request = Net::HTTP::Get.new(uri)
+            request["Accept"] = "application/json"
+            summaryResponse = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
+                http.request(request)
+            }
+            summaryData = JSON.load(summaryResponse.body)
+            if summaryData['short-description'] == "seclab:pub"
+                Jekyll.logger.info "  - #{summary['path']} marked as lab publication"                
+                @publications.data['publications'].push(workGroup)
+                break
+            end
+        end
+    end
+end
+
 
 Jekyll::Hooks.register :site, :post_read do |site|
-    if site.collections['team'] 
-        site.collections['team'].docs.each do |person|
+    @site = site
+    @publications = @site.pages.find{|page| page.permalink == '/publications/'}
+    if ! @publications.data['publications'] 
+        @publications.data['publications'] = []
+    end
+    if @site.collections['team'] 
+        @site.collections['team'].docs.each do |person|
             if person.data['orcid']                
                 Jekyll.logger.info "Requesting ORCID Data for #{person.data['orcid']}"
-                uri = URI("#{API_BASEURL}#{person.data['orcid']}")
+                uri = URI("#{API_BASEURL}/#{person.data['orcid']}")
                 request = Net::HTTP::Get.new(uri)
                 request["Accept"] = "application/json"
                 response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
@@ -24,6 +50,9 @@ Jekyll::Hooks.register :site, :post_read do |site|
                 if image
                     person.data['image'] = image.relative_path 
                 end
+
+                handlePublications(orcData)
+
             end
         end
     end
